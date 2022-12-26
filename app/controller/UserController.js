86b6159602bf7f9217636a38a7a579c41681b907
AkbarMahmudin <wastanami17@gmail.com>
2022-12-26 07:37:28 +0700

    resolve error handling

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -29,7 +29,17 @@ class UserController {
 
       return res.json(statusCode, response)
     } catch (err) {
-      next(err)
+      console.log(err)
+      if (err.code === 'ECONNREFUSED') {
+        return res.status(500).json({ status: 'error', message: 'service unavailable' })
+      }
+
+      const { status = 'fail', message = 'internal server error' } = err.response?.data
+
+      return res.json(err.response?.status || 500, {
+        status,
+        message
+      })
     }
   }
 
