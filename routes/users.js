2f337b3f21408b5072194073a95f3419bbd8cd80
AkbarMahmudin <wastanami17@gmail.com>
2022-12-26 08:46:12 +0700

    feat: my profile

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,9 @@ router.post('/', userController.createUser)
 router.post('/auth', userController.createAuthentication)
 
 router.use(verifyAccessToken)
+router.get('/profiles', userController.getMyProfile)
 router.get('/', userController.getUser)
 router.get('/:id', userController.getUser)
-router.get('/profiles', userController.getMyProfile)
 router.put('/', userController.updateUser)
 
 module.exports = router
